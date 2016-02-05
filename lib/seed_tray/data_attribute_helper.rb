68dd0f966fee47fb547593e98b33ffe4ce1cfbbf
Jeffrey Guenther <guenther.jeffrey@gmail.com>
2016-02-04 23:58:51 -0800

    Fixed gemspec so it will install for testing Dynamically set root object to the name of rails app Tweaked class names to properly match controller names

--- lib/seed_tray/data_attribute_helper.rb
+++ lib/seed_tray/data_attribute_helper.rb
@@ -1,7 +1,7 @@
 module DataAttributeHelper
     def page_data_attr
-        controller_array = controller.controller_path.split("/").map(&:classify).join("_")
-        action = contoller.action_name.classify
+        controller_array = controller.controller_path.split("/").map(&:camelize).join("_")
+        action = controller.action_name.capitalize
         "data-controller='#{controller_array}' data-action='#{action}'".html_safe
     end
 end
--- seed_tray.gemspec
+++ seed_tray.gemspec
@@ -23,7 +23,7 @@ Gem::Specification.new do |spec|
   # end
 
   spec.files         = `git ls-files -z`.split("\x0").reject { |f| f.match(%r{^(test|spec|features)/}) }
-  spec.require_paths = ["lib"]
+  spec.require_paths = Dir["{lib,app}/**/*"]
 
   spec.add_development_dependency "bundler", "~> 1.11"
   spec.add_development_dependency "rake", "~> 10.0"
