b2f3a2f409eb8c0d4b0a760665be2bc892a599eb
Kieran Eglin <kieran.eglin@gmail.com>
2016-04-18 14:10:36 -0700

    Added ability to set console suppress flag

--- lib/seed_tray.rb
+++ lib/seed_tray.rb
@@ -2,4 +2,20 @@ require "seed_tray/version"
 
 module SeedTray
   require 'seed_tray/engine' if defined?(Rails)
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :suppress_console
+  end
+
+  self.configure do |config|
+    config.suppress_console = false
+  end
 end
